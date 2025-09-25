6bd2f7cbd5edea854fa32c42c071ebdcb9c12917
tristenaniyah-hash <tristen.aniyah@gmail.com>
2025-09-24 22:08:19 -0400

    Add files via upload

---
+++ Home_Page.py
@@ -0,0 +1,29 @@
+import streamlit as st
+
+# Title of App
+st.title("Web Development Lab01")
+
+# Assignment Data 
+# TODO: Fill out your team number, section, and team members
+
+st.header("CS 1301")
+st.subheader("Web Development - Section D")
+st.subheader("Tristen Williams")
+
+
+# Introduction
+# TODO: Write a quick description for all of your pages in this lab below, in the form:
+#       1. **Page Name**: Description
+#       2. **Page Name**: Description
+#       3. **Page Name**: Description
+#       4. **Page Name**: Description
+
+st.write("""
+Welcome to my Streamlit Web Development Lab01 app! You can navigate between the pages using the sidebar to the left. The following pages are:
+
+1. **Portfolio**: **A page to highlight my hobbies and skill**
+2. **Project**: **A fun buzzfeed style quiz that I created**
+
+
+""")
+
---
+++ info.py
@@ -0,0 +1,86 @@
+
+#This File will contain the information to be displayed in your portfolio
+
+#CHANGE BELOW
+profile_picture = "Images/profile.png"
+about_me = "My name is Tristen Williams. I'm a first year CS major at Georgia Tech. "
+
+
+#CHANGE BELOW (OPTIONAL)
+linkedin_image_url = "https://content.linkedin.com/content/dam/me/business/en-us/amp/brand-site/v2/bg/LI-Bug.svg.original.svg"
+github_image_url = "https://cdn-icons-png.flaticon.com/256/25/25231.png"
+email_image_url = "https://logowik.com/content/uploads/images/513_email.jpg"
+
+#CHANGE BELOW
+my_linkedin_url = "https://www.linkedin.com/in/tristenwilliamsani/"
+my_github_url = "https://github.com/tristenaniyah-hash"
+my_email_address = "tristen.aniyah@gmail.com"
+
+
+education_data ={
+    'Degree': 'Bachelor of Science in Computer Science',
+    'Institution': 'Georgia Institute of Technology',
+    'Location': 'Atlanta, GA',
+    'Graduation Date': 'May 2029',
+    'GPA': '4.0'
+}
+course_data = {
+    "code":["CS 1301", "MATH 1551","ENGL 1102","PUBP 1142"], 
+    "names":["Intro to CS", "Differential Calculus", "English Composition II", "Teams and Collaboration"], 
+    "semester_taken":["1st","1st","1st","1st"],
+    "skills":["Python","Understanding of differential equations", "Ability to analyze non-human components in a story", "Team collaboration and leadership"], 
+    }
+experience_data = {
+    "Technical Intern at ProTech": (["- Developed and debugged pyhton software for students and my supervisor",
+                                    "- Provided technical support to students during their AWS cloud computing course",
+                                     "- Aided students with their game design projects, helping them develop and debug with Unity"],"Images/coding.jpg"),
+    }
+
+projects_data = {
+    "Fun Quiz!": "Created a short Buzzfeed style quiz to guess the users favorite music artist.",
+}
+
+programming_data = {
+    "Python": 70,
+    "Java": 0,
+    "C": 0,
+}
+
+#CHANGE BELOW (OPTIONAL)
+programming_icons = {
+    "Python": "🐍",
+    "Java": "☕",
+    "C": "🔍",
+}
+spoken_icons = {"French": "🇫🇷",
+    "English": "🇬🇧",
+    "Spanish":"🇪🇸"
+}
+
+#CHANGE BELOW
+spoken_data = {
+    "English": "Fluent",
+    "Spanish": "Beginner",
+}
+leadership_data = {
+    "Student Council Social Media Manager": (["- Designed and managed the Instagram account for Student Council",
+                                              "- Worked on Canva and Photoshop to develop flyers and post them around the school",
+                                              "- Raised awareness of school events and overall turnout"],"Images/council.jpg"),
+    
+    "NJROTC Public Affairs Officer": (["- Designed and the Instagram account foy my platoon",
+                                       "- Designed flyers and posters for the unit to raise awareness and morale",
+                                       "- Collaborated with the yearbook advisor to produce a page displaying the program using Canva"],"Images/PAO.jpg"),
+
+    "NJROTC Academic Team Catpain": (["- Mentored a team of four other cadets to prepare them for competitions",
+                                      "- Checked on the team's progress and memorization of the material, individually and as a group with weekly meetings"],"Images/BB.jpg")
+
+}
+activity_data={
+    "Collaborated with the Veterans Support Group": (["- Raised money for the organization"]),
+    "Collaborated with the Atlanta Community Food Bank": (["- Volunteered on site to sort and package foods and drinks"]),
+    "Collaborated with the Empty Stocking Fun": (["- Raised money for christmas presents for underprivileged chiildren"])
+
+
+
+
+    }
